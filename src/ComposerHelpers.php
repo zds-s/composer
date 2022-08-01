a3bfa9fb1173697768ea9fae8c164245f54aa98a
DeathSatan <2771717608@qq.com>
2022-08-01 09:47:45 +0800

    Fix: 优化

--- src/ComposerHelpers.php
+++ src/ComposerHelpers.php
@@ -12,6 +12,7 @@ namespace SaTan;
 use Composer\Autoload\ClassLoader;
 use Composer\InstalledVersions;
 use OutOfBoundsException;
+use RuntimeException;
 
 class ComposerHelpers
 {
@@ -66,7 +67,7 @@ class ComposerHelpers
                return;
            }
         }
-        throw new \RuntimeException('Class Loader NotFound');
+        throw new RuntimeException('Class Loader NotFound');
     }
 
     /**
@@ -156,8 +157,7 @@ class ComposerHelpers
         return $this->classLoader->getClassMap();
     }
 
-    /**
-     * 检测命名空间是否正确,如果不正确自动补齐
+    /** 检测命名空间是否正确,如果不正确自动补齐
      * @param string $namespace 命名空间
      * @return string
      */
@@ -171,7 +171,7 @@ class ComposerHelpers
      * @param string|array $namespace 命名空间
      * @param bool|string|array $directory 目录|可以是二维数组的目录
      */
-    public function setPsr0(array|string $namespace, $directory = false)
+    public function setPsr0($namespace, $directory = false)
     {
         if (is_array($namespace)) {
             foreach ($namespace as &$name) {
@@ -187,7 +187,7 @@ class ComposerHelpers
      * @param string|array $namespace 命名空间
      * @param bool|string|array $directory 目录|可以是二维数组的目录
      */
-    public function setPsr4(array|string $namespace, $directory = false)
+    public function setPsr4( $namespace, $directory = false)
     {
         if (is_array($namespace)) {
             foreach ($namespace as &$name) {
@@ -215,7 +215,7 @@ class ComposerHelpers
      * @param bool $prepend 是否覆盖在最栈最上方
      * @return void
      */
-    public function addPsr4(array|string $namespace, $directory = null, bool $prepend = false): void
+    public function addPsr4($namespace, $directory = null, bool $prepend = false): void
     {
         if (is_array($namespace) && is_null($directory)) {
             foreach ($namespace as $name => $dir) {
@@ -233,7 +233,7 @@ class ComposerHelpers
      * @param bool $prepend 是否覆盖在最栈最上方
      * @return void
      */
-    public function addPsr0(array|string $namespace, $directory = null, bool $prepend = false): void
+    public function addPsr0($namespace, $directory = null, bool $prepend = false): void
     {
         if (is_array($namespace) && is_null($directory)) {
             foreach ($namespace as $name => $dir) {
@@ -259,7 +259,7 @@ class ComposerHelpers
      * @param string $class
      * @return false|string
      */
-    public function findClassFile(string $class): bool|string
+    public function findClassFile(string $class)
     {
         return $this->classLoader->findFile($class);
     }
